1aa2db1146a7dd90ffc41c550b7cf979748940b0
Bruno Barreto da Cunha Holanda <bbarretoch@gmail.com>
2023-09-13 20:21:10 -0300

    Modulos javascript, composição de classes, tipo de valor e tipo de referência

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor<= 0)
-        {
-         return;
-        }    
-        this._saldo += valor;
-    }
-}
-
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Bruno";
@@ -36,13 +13,15 @@ cliente2.cpf = 888223309;
 const contaCorrenteBruno = new ContaCorrente();
 
 contaCorrenteBruno.agencia = 1001;
+contaCorrenteBruno.cliente = cliente1;
+contaCorrenteBruno.depositar(500);
 
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2
+conta2.agencia = 1002;
 
-contaCorrenteBruno.depositar(-100);
-contaCorrenteBruno.depositar(100);
-contaCorrenteBruno.depositar(100);
+let valor = 200;
+contaCorrenteBruno.transferir(valor, conta2);
 
-const valorSacado = contaCorrenteBruno.sacar(50);
-console.log(valorSacado)
-
+console.log(conta2);
 console.log(contaCorrenteBruno)
